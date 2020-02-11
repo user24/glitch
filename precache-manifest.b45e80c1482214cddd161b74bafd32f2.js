21de82e03079e470166379abae03a0f06244ff25
Howard Yeend <h.yeend@growinginteractive.com>
2020-02-11 16:13:25 +0000

    Updates

---
+++ precache-manifest.b45e80c1482214cddd161b74bafd32f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cea5eb4f94511363d7a881b588d5964",
+    "url": "/glitch/index.html"
+  },
+  {
+    "revision": "4960a9ce54d742d64160",
+    "url": "/glitch/static/css/main.e3ce4fa0.chunk.css"
+  },
+  {
+    "revision": "d4a03c767e436bcabbf2",
+    "url": "/glitch/static/js/2.305f0bc7.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/glitch/static/js/2.305f0bc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4960a9ce54d742d64160",
+    "url": "/glitch/static/js/main.78db0d70.chunk.js"
+  },
+  {
+    "revision": "8af4ae9ad4dd8820f7f7",
+    "url": "/glitch/static/js/runtime-main.f0743730.js"
+  }
+]);
